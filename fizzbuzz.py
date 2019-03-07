274899a775265626d4d20defef4e02a51eefec0f
Simon <01secn@gmail.com>
2019-03-07 10:34:16 +0000

    Moved logic to check if number is divisible by 3 and by 5 into separate methods, so that fizzbuzz method is SRP and is clearer (with method names providing abstraction)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,17 @@
 def fizzbuzz(number):
-    if number % 5 == 0 and number % 3 == 0:
+    if divBy5(number) and divBy3(number):
         return 'fizzbuzz'
-    if number % 3 == 0:
+    if divBy3(number):
         return 'fizz'
-    if number % 5 == 0:
+    if divBy5(number):
         return 'buzz'
     else:
         return number
+
+
+def divBy3(number):
+    return number % 3 == 0
+
+
+def divBy5(number):
+    return number % 5 == 0
