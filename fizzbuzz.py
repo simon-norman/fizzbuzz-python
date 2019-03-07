4ae9d86d3c1f28988214500f3658e625041fede3
Simon <01secn@gmail.com>
2019-03-07 10:29:17 +0000

    Multiple of 3 and 5 returns fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,6 @@
 def fizzbuzz(number):
+    if number % 5 == 0 and number % 3 == 0:
+        return 'fizzbuzz'
     if number % 3 == 0:
         return 'fizz'
     if number % 5 == 0:
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -20,3 +20,13 @@ def test_returns_fizz_on_mult_of_3():
 @pytest.mark.unittest
 def test_returns_buzz_on_5():
     assert fizzbuzz(5) == 'buzz'
+
+
+@pytest.mark.unittest
+def test_returns_buzz_on_mult_of_5():
+    assert fizzbuzz(25) == 'buzz'
+
+
+@pytest.mark.unittest
+def test_returns_fizzbuzz_on_mult_3_and_5():
+    assert fizzbuzz(15) == 'fizzbuzz'
